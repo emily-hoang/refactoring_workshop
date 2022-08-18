393e0e4e2311c2487c0f3185bb268fca5cf34973
emily-hoang <nhung@nhunghoang.com>
2022-08-18 11:22:18 +1000

    Adds tests for customer statement method

---
+++ src/README.md
@@ -0,0 +1,14 @@
+### First step
+- adding tests:
+  - prevent ourselves from introducing bugs,
+  - accidentally remove any of the existing logic
+  - break the program
+  - and the most importantly, they give us fast feedback and the confident that we're heading to the right direction
+  - the tests are also the living document which developers can refer to to get a basic picture of the logic of the module and the system as a whole.
+
+### Second step
+- decomposing the statement function
+  - extracting the switch statement  into its own function
+  - run the tests
+  - change the var "thisAmount" to "result" to make it clearer
+  - run the tests
--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -4,12 +4,76 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CustomerTest {
+    private static final String CUSTOMER_NAME = "John";
+    private static final String REGULAR_MOVIE_NAME = "The IT Crowd";
+    private static final String NEW_RELEASE__MOVIE_NAME = "RRR";
+    private static final String CHILDREN_MOVIE_NAME = "Frozen";
     @Test
-    public void test() {
+    public void outputStatementForARegularMovie() {
+        Customer customer = new Customer(CUSTOMER_NAME);
+        Movie regularMovie = new Movie("The IT Crowd", 0);
+        Rental rental = new Rental(regularMovie, 2);
 
-        int expected = 3;
+        customer.addRental(rental);
 
-        Assert.assertEquals(expected, 1 + 2);
+        String actualStatement = customer.statement();
+        String expectedStatement = "Rental Record for " + CUSTOMER_NAME + "\n";
+        expectedStatement += "\t" + REGULAR_MOVIE_NAME + "\t2.0\n";
+        expectedStatement += "Amount owed is 2.0\n";
+        expectedStatement += "You earned 1 frequent renter points";
+
+        Assert.assertEquals(actualStatement, expectedStatement);
+    }
+
+    @Test
+    public void outputStatementForANewReleaseMovieWithMoreThan1DayRented() {
+        Customer customer = new Customer(CUSTOMER_NAME);
+        Movie newReleaseMovie = new Movie(NEW_RELEASE__MOVIE_NAME, 1);
+        Rental rental = new Rental(newReleaseMovie, 3);
+
+        customer.addRental(rental);
+
+        String actualStatement = customer.statement();
+        String expectedStatement = "Rental Record for " + CUSTOMER_NAME + "\n";
+        expectedStatement += "\t" + NEW_RELEASE__MOVIE_NAME +"\t9.0\n";
+        expectedStatement += "Amount owed is 9.0\n";
+        expectedStatement += "You earned 2 frequent renter points";
+
+        Assert.assertEquals(actualStatement, expectedStatement);
+    }
+
+    @Test
+    public void outputStatementForANewReleaseMovieWithLessThan1DayRented() {
+        Customer customer = new Customer(CUSTOMER_NAME);
+        Movie newReleaseMovie = new Movie(NEW_RELEASE__MOVIE_NAME, 1);
+        Rental rental = new Rental(newReleaseMovie, 1);
+
+        customer.addRental(rental);
+
+        String actualStatement = customer.statement();
+        String expectedStatement = "Rental Record for " + CUSTOMER_NAME + "\n";
+        expectedStatement += "\t" + NEW_RELEASE__MOVIE_NAME + "\t3.0\n";
+        expectedStatement += "Amount owed is 3.0\n";
+        expectedStatement += "You earned 1 frequent renter points";
+
+        Assert.assertEquals(actualStatement, expectedStatement);
+    }
+
+    @Test
+    public void outputStatementForAChildrenMovie() {
+        Customer customer = new Customer(CUSTOMER_NAME);
+        Movie newReleaseMovie = new Movie(CHILDREN_MOVIE_NAME, 1);
+        Rental rental = new Rental(newReleaseMovie, 3);
+
+        customer.addRental(rental);
+
+        String actualStatement = customer.statement();
+        String expectedStatement = "Rental Record for " + CUSTOMER_NAME + "\n";
+        expectedStatement += "\t" + CHILDREN_MOVIE_NAME + "\t9.0\n";
+        expectedStatement += "Amount owed is 9.0\n";
+        expectedStatement += "You earned 2 frequent renter points";
+
+        Assert.assertEquals(actualStatement, expectedStatement);
     }
 
 }
